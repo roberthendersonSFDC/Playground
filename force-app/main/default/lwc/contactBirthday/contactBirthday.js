dbad285c829d84be9d4410a66a726ca2ff6098a0
Robert Henderson <robert.henderson@salesforce.com>
2021-04-12 16:48:28 -0700

    Added property customization and styles

--- force-app/main/default/lwc/contactBirthday/contactBirthday.js
+++ force-app/main/default/lwc/contactBirthday/contactBirthday.js
@@ -22,17 +22,78 @@ const NOW = new Date();
 export default class ContactBirthday extends LightningElement {
   @api recordId;
   @api withinDays;
+  @api
+  set birthdayAnnouncementLabel(value) {
+    this.labels.birthdayAnnouncementLabel = value;
+  }
+  get birthdayAnnouncementLabel() {
+    return this.labels.birthdayAnnouncementLabel;
+  }
 
-  emailButtonIconName = "utility:email";
-  cardButtonIconName = "utility:send";
+  @api
+  set sendEmailButtonLabel(value) {
+    this.labels.sendEmailButtonLabel = value;
+  }
+  get sendEmailButtonLabel() {
+    return this.labels.sendEmailButtonLabel;
+  }
 
-  emailButtonTitle = `Send ${this.firstname} a Birthday email`;
-  cardButtonTitle = `Send ${this.firstname} a Birthday card`;
+  @api
+  set sendCardButtonLabel(value) {
+    this.labels.sendCardButtonLabel = value;
+  }
+  get sendCardButtonLabel() {
+    return this.labels.sendCardButtonLabel;
+  }
+
+  @api
+  set sendEmailToastHeaderLabel(value) {
+    this.labels.sendEmailToastHeaderLabel = value;
+  }
+  get sendEmailToastHeaderLabel() {
+    return this.labels.sendEmailToastHeaderLabel;
+  }
+
+  @api
+  set sendEmailToastMessageLabel(value) {
+    this.labels.sendEmailToastMessageLabel = value;
+  }
+  get sendEmailToastMessageLabel() {
+    return this.labels.sendEmailToastMessageLabel;
+  }
+
+  @api
+  set sendCardToastHeaderLabel(value) {
+    this.labels.sendCardToastHeaderLabel = value;
+  }
+  get sendCardToastHeaderLabel() {
+    return this.labels.sendCardToastHeaderLabel;
+  }
+
+  @api
+  set sendCardToastMessageLabel(value) {
+    this.labels.sendCardToastMessageLabel = value;
+  }
+  get sendCardToastMessageLabel() {
+    return this.labels.sendCardToastMessageLabel;
+  }
+
+  labels = {
+    birthdayAnnouncementLabel: "",
+    sendEmailButtonLabel: "",
+    sendCardButtonLabel: "",
+    sendEmailToastHeaderLabel: "",
+    sendEmailToastMessageLabel: "",
+    sendCardToastHeaderLabel: "",
+    sendCardToastMessageLabel: ""
+  };
 
   contact;
   firstname;
   birthday;
   showComponent;
+  buttonState;
+
   @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
   wiredRecord({ error, data }) {
     if (error) {
@@ -58,6 +119,21 @@ export default class ContactBirthday extends LightningElement {
       this.showComponent = this.isDateUpcoming(
         this.contact.fields.Birthdate.value
       );
+      this.addDataToLabels();
+      this.buttonState = this.getInitialButtonState();
+    }
+  }
+
+  addDataToLabels() {
+    for (const property in this.labels) {
+      let label = this.labels[property].slice();
+      if (label.indexOf("{FirstName}") > -1) {
+        label = label.replaceAll("{FirstName}", this.firstname);
+      }
+      if (label.indexOf("{Birthdate}") > -1) {
+        label = label.replaceAll("{Birthdate}", this.birthday);
+      }
+      this.labels[property] = label;
     }
   }
 
@@ -82,22 +158,60 @@ export default class ContactBirthday extends LightningElement {
     return birthdayString;
   }
 
+  getInitialButtonState() {
+    return {
+      email: {
+        label: this.labels.sendEmailButtonLabel,
+        title: `Send ${this.firstname} a Birthday email`,
+        iconName: "utility:email",
+        disabled: false
+      },
+      card: {
+        label: this.labels.sendCardButtonLabel,
+        title: `Send ${this.firstname} a Birthday card`,
+        iconName: "utility:send",
+        disabled: false
+      }
+    };
+  }
+
+  replaceDataPlaceholders(value) {
+    let formattedValue = value.slice();
+    if (formattedValue.indexOf("{FirstName}") > -1) {
+      formattedValue.replaceAll("{FirstName}", this.firstname);
+    }
+    if (formattedValue.indexOf("{Birthdate}") > -1) {
+      formattedValue.replaceAll("{Birthdate}", this.birthday);
+    }
+    return formattedValue;
+  }
+
   handleEmailButtonClick() {
+    let newState = Object.assign({}, this.getInitialButtonState());
+    newState.email.disabled = true;
+    newState.email.iconName = "utility:check";
+    this.buttonState = newState;
     this.showSuccessToast(
+      this.labels.sendEmailToastHeaderLabel,
       `A Happy Birthday Email has been sent to ${this.firstname}`
     );
   }
 
   handleCardButtonClick() {
+    let newState = Object.assign({}, this.getInitialButtonState());
+    newState.card.disabled = true;
+    newState.card.iconName = "utility:check";
+    this.buttonState = newState;
     this.showSuccessToast(
+      this.labels.sendCardToastHeaderLabel,
       `A Happy Birthday Card has been mailed to ${this.firstname}`
     );
   }
 
-  showSuccessToast(message) {
+  showSuccessToast(header, message) {
     this.dispatchEvent(
       new ShowToastEvent({
-        title: "Birthday Success!",
+        title: header,
         message,
         variant: "success"
       })
